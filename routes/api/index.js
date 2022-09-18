087232cb87aef0da9dcd654659b4b28187fd297d
Battlehart <sarah.stelljes@gmail.com>
2022-09-18 16:26:20 -0400

    added comment functionality in back-end. Also updated some other files such as pizza related stuff.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
 
 // add prefix of `/pizza` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+// for comments
+router.use('/comments', commentRoutes);
 
 module.exports = router;
